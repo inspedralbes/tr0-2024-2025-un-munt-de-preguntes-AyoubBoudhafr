8249eadaf24bd4a2d62fd1bd0071e00a37b0f349
Ayoub <a23mohbounij@inspedralbes.cat>
2024-09-20 12:17:20 +0200

    ya imprime todas las preguntas

--- back/getPreguntes.php
+++ back/getPreguntes.php
@@ -1,5 +1,5 @@
 <?php
-session_start();
+ session_start();
 
 $contPreg = 20;
     header('Content-Type: application/json');
@@ -18,79 +18,14 @@ function desordenarPreguntas($preguntas, $contPreg){
     return $preguntasDesordenadas;
 }
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    if (isset($_POST['reiniciar'])) {
-        session_unset();
-        header('Location: index.php');
-        exit;
-    }
-
-    if (isset($_POST['resposta'])) {
-        $preguntaActual = $_SESSION['preguntaActual'];
-        $respostaSeleccionada = intval($_POST['resposta']);
-
-        if ($respostaSeleccionada == $_SESSION['preguntes'][$preguntaActual]['resposta_correcta']) {
-            $_SESSION['puntuacio']++;
-            $_SESSION['resultats'][] = true;
-        } else {
-            $_SESSION['resultats'][] = false;
-        }
-    
-        $_SESSION['preguntaActual']++;
-        
-        if ($_SESSION['preguntaActual'] >= count($_SESSION['preguntes'])) {
-            header('Location: resultats.php');
-            exit;
-        } else {
-            header('Location: index.php');
-            exit;
-        }
-    }
-} else {
-    if (!isset($_SESSION['preguntes'])) {
-        $_SESSION['preguntes'] = desordenarPreguntas($array_preguntas, $contPreg);
-        $_SESSION['preguntaActual'] = 0;
-        $_SESSION['puntuacio'] = 0;
-        $_SESSION['resultats'] = [];
-    }
-
-    $preguntaActual = $_SESSION['preguntaActual'];
-    $pregunta = $_SESSION['preguntes'][$preguntaActual];
+$totRespostes=[];
+foreach ($array_preguntas as $senseResposta){
+    $totRespostes[] = array(
+        'id' => $senseResposta['id'],
+        'pregunta' => $senseResposta['pregunta'],
+        'respostes' => $senseResposta['respostes']
+    );
 }
+$totRespostes2=json_encode(["preguntes"=>$totRespostes]);
+echo $totRespostes2;
 ?>
-
-<!-- <?php 
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['resposta'])) {
-    $respuestaCorrecta = $_SESSION['preguntes'][$_SESSION['preguntaActual'] - 1]['resposta_correcta'];
-    
-    if ($_POST['resposta'] == $respuestaCorrecta) {
-        $mensaje = '¡Correcto!';
-    } else {
-        $mensaje = 'Incorrecto.';
-    }
-} 
-
-if (isset($pregunta)) {
-    ?>
-    <h2><?php echo htmlspecialchars($pregunta['pregunta']); ?></h2>
-    <img src="<?php echo htmlspecialchars($pregunta['imatge']); ?>" alt="Imagen de la pregunta" style="height: 200px;">
-    <form method="post">
-        <?php 
-        foreach ($pregunta['respostes'] as $resposta) { ?>
-            <p>
-                <button name="resposta" value="<?php echo htmlspecialchars($resposta['id']); ?>">
-                    <?php echo htmlspecialchars($resposta['etiqueta']); ?>
-                </button>
-            </p>
-        <?php } ?>
-    </form>
-    <?php
-} else {
-    echo '<p>No hay preguntas disponibles.</p>';
-}
-?>
-
-<form method="post">
-    <button type="submit" name="reiniciar" style="background-color: red;">Reiniciar el juego</button>
-</form>
- -->
--- web/js/utils.js
+++ web/js/utils.js
@@ -1,23 +1,21 @@
+
+fetch('http://localhost/tr0-2024-2025-un-munt-de-preguntes-AyoubBoudhafr/back/getPreguntes.php')
+.then(response => response.json())
+.then(data => {
+    console.log(data)
+    data.preguntes.forEach((pregunta, indexPregunta) => {
+        let preguntaHtml = `
+
+        <h2>${pregunta.pregunta}</h2>
+        <img src="${pregunta.imatge}" alt="Image" style="max-width: 100%; height: 200px;">`;
+        
+        preguntaHtml += pregunta.respostes.map((resposta, indexResposta) => `
+        <p><button onclick="BuscarRespuesta(${indexPregunta}, ${indexResposta})">${resposta.etiqueta}</button></p>`).join('');
+        preguntaHtml += '<hr>';
+
+        document.body.innerHTML += preguntaHtml;
+    });
+});
 function BuscarRespuesta(preguntaIndex, respostaIndex) {
     console.log(`Pregunta número: ${preguntaIndex + 1}, Resposta número: ${respostaIndex + 1}`);
 }
-fetch('../../back/preguntas.json')
-    .then(response => response.json())
-    .then(data => {
-        
-        data.preguntes.forEach((pregunta, indexPregunta) => {
-            let preguntaHtml = `
-                <h2>${pregunta.pregunta}</h2>
-                <img src="${pregunta.imatge}" alt="Image" style="max-width: 100%; height: 200px;">
-            `;
-
-            preguntaHtml += pregunta.respostes.map((resposta, indexResposta) => `
-                <p><button onclick="BuscarRespuesta(${indexPregunta}, ${indexResposta})">${resposta.etiqueta}</button></p>
-            `).join('');
-
-            preguntaHtml += '<hr>';
-
-            document.body.innerHTML += preguntaHtml;
-        });
-    });
-    
