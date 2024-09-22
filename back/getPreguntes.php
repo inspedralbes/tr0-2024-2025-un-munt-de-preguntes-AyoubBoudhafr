5560a958b168baa6fb9ce921a61305ce8c5d0bdb
AyoubBoudhafr <a23mohbounij@inspedralbes.cat>
2024-09-22 22:59:05 +0200

    funcion de desordenar mejorada

--- back/getPreguntes.php
+++ back/getPreguntes.php
@@ -1,32 +1,24 @@
 <?php
- session_start();
+session_start();
 
-$contPreg = 20;
-    header('Content-Type: application/json');
-    $json = file_get_contents("preguntas.json");
-    $array_preguntas = json_decode($json, true)['preguntes'];
+header('Content-Type: application/json');
 
+$json = file_get_contents("preguntas.json");
+$array_preguntas = json_decode($json, true)['preguntes'];
 
-function desordenarPreguntas($preguntas, $contPreg){
-    $preguntasDesordenadas = array();
+shuffle($array_preguntas);
+$preguntasDesordenadas = array_slice($array_preguntas, 0, 10);
 
-    for ($i = 0; $i < 10; $i++) {
-        $num = rand(0, count($preguntas) - 1);
-        array_push($preguntasDesordenadas, $preguntas[$num]);
-        array_shift($preguntas[$num]);
-    }
-    return $preguntasDesordenadas;
-}
-
-$totRespostes=[];
-foreach ($array_preguntas as $senseResposta){
+$totRespostes = [];
+foreach ($preguntasDesordenadas as $senseResposta) {
     $totRespostes[] = array(
         'id' => $senseResposta['id'],
         'pregunta' => $senseResposta['pregunta'],
         'respostes' => $senseResposta['respostes'],
-        'imatge'=> $senseResposta['imatge'],
+        'imatge' => $senseResposta['imatge'],
     );
 }
-$totRespostes2=json_encode(["preguntes"=>$totRespostes]);
+
+$totRespostes2 = json_encode(["preguntes" => $totRespostes]);
 echo $totRespostes2;
 ?>
