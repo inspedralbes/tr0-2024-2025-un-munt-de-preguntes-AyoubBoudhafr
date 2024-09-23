2e4d4496fba636afab99ece0f7fa93aee141758a
Ayoub <a23mohbounij@inspedralbes.cat>
2024-09-23 10:29:50 +0200

    boton restart arreglado pero ahora no va el pasar de pregunta

--- web/js/utils.js
+++ web/js/utils.js
@@ -1,12 +1,18 @@
 let dataGlobal;
 
-fetch("http://localhost/tr0-2024-2025-un-munt-de-preguntes-AyoubBoudhafr/back/getPreguntes.php")
-    .then(resp => resp.json())
-    .then(data => {
-        dataGlobal = data; 
+obtPreg(dataGlobal);
+
+function obtPreg(dataGlobal){
+    let json = "../back/getPreguntes.php";
+    fetch (json)
+   .then(resp => resp.json())
+   .then(data =>{
+    dataGlobal = data;
         let pregActual = 0;
         mostrarPregunta(dataGlobal, pregActual);
-    });
+   })
+}
+
 
 function mostrarPregunta(data, pregActual) {
     let salida = '';
@@ -14,21 +20,25 @@ function mostrarPregunta(data, pregActual) {
 
     if (pregActual < totPreguntas.length) {
         let pregunta = totPreguntas[pregActual].pregunta;
+        salida += '<h2>Pregunta numero '+(pregActual+1)+'';
         salida += '<h3>' + pregunta + '</h3>';
         salida += '<img style="height: 200px" src="' + totPreguntas[pregActual].imatge + '">';
         let respuestas = totPreguntas[pregActual].respostes; 
         
         for (let j = 0; j < respuestas.length; j++) {
-            salida += '<br><button onclick="siguientePregunta(' + (pregActual + 1) + ', \'' + (j+1) + '\')">' + respuestas[j].etiqueta + '</button>';
+            salida += '<br><button onclick="siguientePregunta(' + (pregActual + 1) + ', \'' + (j+1) + '\'\''+data+'\')">' + respuestas[j].etiqueta + '</button>';
         }
-        
+        salida += '<br><button onclick="resetTest('+data+')">Restart</button>'
         document.getElementById("test").innerHTML = salida;
     } else {
         document.write("Se acabaron las preguntas!!")
     }
 }
 
-function siguientePregunta(pregActual, respuesta) {
-    alert('Respuesta seleccionada: ' + respuesta);
-    mostrarPregunta(dataGlobal, pregActual); 
+function siguientePregunta(pregActual, respuesta, dataGlobal) {
+    console.log('Respuesta seleccionada: ' + respuesta);
+    mostrarPregunta(dataGlobal, pregActual);
+}
+function resetTest(dataGlobal){
+   obtPreg(dataGlobal);
 }
