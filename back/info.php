4191f4b9180bb57b4482cc9d57b82d3fcbf427c6
AyoubBoudhafr <a23mohbounij@inspedralbes.cat>
2024-10-02 23:33:27 +0200

    creado info.php para mandar toda la informacion de la base de datos al front y que la pueda imprimir

---
+++ back/info.php
@@ -0,0 +1,33 @@
+<?php
+session_start();
+require_once("conexio.php");
+
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$resultRespostes = mysqli_query($conn, "SELECT * FROM preguntes"); 
+$info = $resultRespostes->fetch_all(MYSQLI_ASSOC);
+
+$arrayEnvio = [];
+foreach ($info as $preguntes) {
+    $arrayEnvio[] = [
+        "id" => $preguntes["id"],
+        "pregunta" => $preguntes["pregunta"],
+        "opciones" => [
+            $preguntes["pregunta_1"],
+            $preguntes["pregunta_2"],
+            $preguntes["pregunta_3"],
+            $preguntes["pregunta_4"],
+        ],
+        "correcta" => $preguntes["resposta_correcta"]
+        ];
+    }
+
+$_SESSION['preguntes'] = $arrayEnvio;
+
+echo json_encode($_SESSION['preguntes']);
+
+mysqli_close($conn);
