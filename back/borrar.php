f555530e84530ef0d2af9f9dac085cad6b104232
AyoubBoudhafr <a23mohbounij@inspedralbes.cat>
2024-10-02 23:31:59 +0200

    creado borrar.php para borrar preguntas seleccionadas por el cliente en la base de datos

---
+++ back/borrar.php
@@ -0,0 +1,30 @@
+<?php
+require_once('conexio.php');
+
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$data = json_decode(file_get_contents("php://input"), true);
+$id = $data['id'];
+
+if ($id) {
+    $stmt = $conn->prepare("DELETE FROM preguntes WHERE id = ?");
+    $stmt->bind_param("i", $id); // "i" indica que el parámetro es un entero
+
+    if ($stmt->execute()) {
+        echo json_encode(["success" => true, "message" => "Pregunta borrada con éxito."]);
+    } else {
+        echo json_encode(["success" => false, "message" => "Error al borrar la pregunta: " . $stmt->error]);
+    }
+
+    $stmt->close();
+} else {
+    echo json_encode(["success" => false, "message" => "ID no válido."]);
+}
+
+// Cerrar la conexión
+$conn->close();
+?>
